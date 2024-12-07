a76e50ce42c561584f053b1e5163d74cc5f76204
Roger <anhth5659@gmail.com>
2024-12-07 10:35:04 +0700

    fix menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import List from './pages/List'
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/list" element={<List />} />
     </Routes>
   )
 }
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 const Menu = () => {
+  const location = useLocation();
+  const [pressedState, setPressedState] = React.useState(location.pathname);
+
+  const handleButtonClick = (path) => {
+    setPressedState(path);
+  };
+
   return (
     <div className="w-[350px] flex flex-col h-full justify-center bg-[#D9D9D9] bg-opacity-10">
-        <button className="menu">Lập danh sách khám bệnh</button>
-        <button className="menu">Lập phiếu khám bệnh</button>
-        <button className="menu">Tra cứu bệnh nhân</button>
-        <button className="menu">Lập hóa đơn thanh toán</button>
-        <button className="menu">Lập báo cáo tháng</button>
-        <button className="menu">Thay đổi quy định</button>
+      <Link to="/list" onClick={() => handleButtonClick('/list')} className='menu'>
+        <button>Lập danh sách khám bệnh</button>
+      </Link>
+      <Link to="/create" onClick={() => handleButtonClick('/create')} className={pressedState === '/create' ? 'menu-pressed' : 'menu'}>
+        <button>Lập phiếu khám bệnh</button>
+      </Link>
+      <Link to="/search" onClick={() => handleButtonClick('/search')} className={pressedState === '/create' ? 'menu-pressed' : 'menu'}>
+        <button>Tra cứu bệnh nhân</button>
+      </Link>
+      <Link to="/invoice" onClick={() => handleButtonClick('/invoice')} className={pressedState === '/create' ? 'menu-pressed' : 'menu'}>
+        <button>Lập hóa đơn thanh toán</button>
+      </Link>
+      <Link to="/report" onClick={() => handleButtonClick('/report')} className={pressedState === '/create' ? 'menu-pressed' : 'menu'}>
+        <button>Lập báo cáo tháng</button>
+      </Link>
+      <Link to="/rules" onClick={() => handleButtonClick('/rules')} className={pressedState === '/create' ? 'menu-pressed' : 'menu'}>
+        <button>Thay đổi quy định</button>
+      </Link>
     </div>
-  )
+  );
 }
 
-export default Menu
+export default Menu;
