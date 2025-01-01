41c5473386ea584e61df7e19106d45bf91c06397
Roger <anhth5659@gmail.com>
2025-01-01 23:22:01 +0700

    fix cors

--- src/components/NameAndLogo.jsx
+++ src/components/NameAndLogo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const NameAndLogo = () => {
   return (
     <div className='flex flex-row m-3 items-center'>
-        <img className='mx-[22px]' src='./logo110.png'/>
+        <img className='mx-[22px]' src='logo110.png'/>
         <div className='text-5xl font-bold text-black'>Phòng khám X</div>
     </div>
   )
